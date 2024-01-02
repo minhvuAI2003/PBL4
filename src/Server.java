fc7e2c5bfdb90ff9a64a89adaa317551341d3875
khanhhan20033 <106676797+khanhhan20033@users.noreply.github.com>
2024-01-02 17:12:43 +0700

    Update Server.java
    yes

--- src/Server.java
+++ src/Server.java
@@ -19,9 +19,12 @@ public class Server {
 	private static String jdbcUsername = "root";
 	private static String jdbcPassword = "root";
 	ServerSocket ss;
+	Map<String, Socket> socketss = new HashMap<String, Socket>();
+
 	Map<String, Socket> sockets = new HashMap<String, Socket>();
 	Vector<client> Clientlist = new Vector<client>();
 	String[][] Message = new String[1000][1000];
+	String[][] block = new String[1000][1000];
 	Map<String, int[]> ID = new HashMap<String, int[]>();
 	Vector<client> OnlineClient = new Vector<client>();
 
@@ -32,16 +35,74 @@ public class Server {
 			// int index = 0;
 
 			try {
-
 				Socket sc = ss.accept();
 				System.out.println(sc);
 				DataInputStream dis = new DataInputStream(sc.getInputStream());
 				String receive = dis.readUTF();
+				if (receive.equals("block")) {
+					new Thread(() -> {
+						String ten1 = "";
+
+						try {
+							String status = dis.readUTF();
+							String ten = dis.readUTF();
+							ten1 = dis.readUTF();
+
+							System.out.println(ten + " " + ten1);
+							Vector<client> vector1 = fetchalluser(sockets, ID);
+							Clientlist.clear();
+							socketss.put(ten1, sc);
+							Clientlist = vector1;
+							int i1 = 0, i2 = 0;
+							for (int i = 0; i < Clientlist.size(); i++) {
+								if (Clientlist.get(i).gettk().equals(ten))
+									i1 = i;
+								if (Clientlist.get(i).gettk().equals(ten1))
+									i2 = i;
+							}
+							if (checkblock(Clientlist.get(i2).gettk(), Clientlist.get(i1).gettk())) {
+								this.updateblock(ten1, ten, status);
+								block[Clientlist.get(i2).getid()][Clientlist.get(i1).getid()] = status;
+							} else {
+								this.insert_block(ten1, ten);
+								this.updateblock(ten1, ten, status);
+								block[Clientlist.get(i2).getid()][Clientlist.get(i1).getid()] = status;
+
+							}
+							if (sockets.get(Clientlist.get(i1).gettk()) != null)
+								if (kiemtra(Clientlist, Clientlist.get(i1).sc, i1)) {
+
+									DataOutputStream dos1 = new DataOutputStream(
+											Clientlist.get(i1).sc.getOutputStream());
+									dos1.writeUTF(Message[Clientlist.get(i1).getid()][Clientlist.get(i2).getid()]);
+									// System.out.println(
+									// Message[Clientlist.get(idd).getid()][Clientlist.get(b).getid()]);
+									dos1.writeUTF(Clientlist.get(i2).gettk());
+									if (status.equals("true"))
+										dos1.writeUTF("block");
+									else
+										dos1.writeUTF("unblock");
+
+									// System.out.println(Clientlist.get(b).gettk());
+
+								}
+							
+						} catch (IOException e) {
+							socketss.put(ten1, null);
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (SQLException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}).start();
+				}
 				if (receive.equals("dndk")) {
 					client t = new client(sc);
 
 					t.start();
-				} else {
+				}
+				if (receive.equals("status")) {
 					new Thread(() -> {
 
 						boolean loop = true;
@@ -58,6 +119,7 @@ public class Server {
 								} else {
 									dos.writeUTF("hello");
 								}
+
 							} catch (IOException e) {
 								// TODO Auto-generated catch block
 								e.printStackTrace();
@@ -91,6 +153,16 @@ public class Server {
 		Connection connection = getConnection();
 		PreparedStatement preparedStatement = connection
 				.prepareStatement("update message set content=?  where sender=? and receiver=? ");
+		preparedStatement.setString(1, message);
+		preparedStatement.setString(2, sender);
+		preparedStatement.setString(3, receiver);
+		preparedStatement.executeUpdate();
+	}
+
+	public void updateblock(String sender, String receiver, String message) throws SQLException {
+		Connection connection = getConnection();
+		PreparedStatement preparedStatement = connection
+				.prepareStatement("update block set status=?  where sender=? and receiver=? ");
 		preparedStatement.setString(1, message);
 		preparedStatement.setString(2, sender);
 		preparedStatement.setString(3, receiver);
@@ -193,6 +265,28 @@ public class Server {
 		return rs.next();
 	}
 
+	public boolean checkblock(String gettk, String gettk2) throws SQLException {
+		// TODO Auto-generated method stub
+
+		Connection connection = getConnection();
+		PreparedStatement preparedStatement = connection
+				.prepareStatement("select * from block where sender=? and receiver=?");
+		preparedStatement.setString(1, gettk);
+		preparedStatement.setString(2, gettk2);
+		ResultSet rs = preparedStatement.executeQuery();
+
+		return rs.next();
+	}
+
+	public void insert_block(String gettk, String gettk2) throws SQLException {
+		// TODO Auto-generated method stub
+		Connection connection = getConnection();
+		PreparedStatement preparedStatement = connection.prepareStatement("insert into block values(?,?,'false')");
+		preparedStatement.setString(1, gettk);
+		preparedStatement.setString(2, gettk2);
+		preparedStatement.execute();
+	}
+
 	private String mess(String gettk, String gettk2) throws SQLException {
 
 		String message = "";
@@ -208,13 +302,27 @@ public class Server {
 		return message;
 	}
 
+	public String getblockstatus(String client, String client2) throws SQLException {
+		// TODO Auto-generated method stub
+		String message = "";
+		Connection connection = getConnection();
+		PreparedStatement preparedStatement = connection
+				.prepareStatement("select status from block where sender=? and receiver=?");
+		preparedStatement.setString(1, client);
+		preparedStatement.setString(2, client2);
+		ResultSet rs = preparedStatement.executeQuery();
+		while (rs.next()) {
+			message += rs.getString("status");
+		}
+		return message;
+	}
+
 	public static void main(String[] args) throws IOException, SQLException {
 		new Server();
 	}
 
 	class client extends Thread {
 		int id;
-
 		private String tk;
 		private String mk;
 
@@ -330,6 +438,17 @@ public class Server {
 							} else
 								b += i;
 						}
+						for (int i = 0; i < Clientlist.size(); i++)
+							for (int j = 0; j < Clientlist.size(); j++)
+								if (i != j) {
+									if (checkblock(Clientlist.get(i).gettk(), Clientlist.get(j).gettk()))
+										block[Clientlist.get(i).getid()][Clientlist.get(j).getid()] = getblockstatus(
+												Clientlist.get(i).gettk(), Clientlist.get(j).gettk());
+									else {
+										block[Clientlist.get(i).getid()][Clientlist.get(j).getid()] = "false";
+										insert_block(Clientlist.get(i).gettk(), Clientlist.get(j).gettk());
+									}
+								}
 						for (int i = 0; i < Clientlist.size(); i++) {
 							if (!Clientlist.get(i).gettk().equals(tk)) {
 								if (!checkcouple(tk, Clientlist.get(i).gettk())) {
@@ -341,6 +460,16 @@ public class Server {
 											Clientlist.get(i).gettk());
 									dos.writeUTF(Message[Clientlist.get(b).getid()][Clientlist.get(i).getid()]);
 								}
+							}
+						}
+						for (int i = 0; i < Clientlist.size(); i++) {
+							if (!Clientlist.get(i).gettk().equals(tk)) {
+								dos.writeUTF(block[Clientlist.get(b).getid()][Clientlist.get(i).getid()]);
+							}
+						}
+						for (int i = 0; i < Clientlist.size(); i++) {
+							if (!Clientlist.get(i).gettk().equals(tk)) {
+								dos.writeUTF(block[Clientlist.get(i).getid()][Clientlist.get(b).getid()]);
 							}
 						}
 						for (int i = 0; i < Clientlist.size(); i++)
@@ -381,33 +510,36 @@ public class Server {
 											break;
 										}
 									}
+
 									String message = (String) Message[Clientlist.get(b).getid()][Clientlist.get(idd)
 											.getid()];
 									if (!message.equals(""))
 										message += "\r\n\r\n" + tr;
 									else
 										message = tr;
-									Message[Clientlist.get(b).getid()][Clientlist.get(idd).getid()] = message;
-									System.out.println(Message[Clientlist.get(b).getid()][Clientlist.get(idd).getid()]);
-									update(Clientlist.get(b).gettk(), Clientlist.get(idd).gettk(), message);
+									// System.out.println(Message[Clientlist.get(b).getid()][Clientlist.get(idd).getid()]);
+									if (block[Clientlist.get(b).getid()][Clientlist.get(idd).getid()].equals("false")
+											&& block[Clientlist.get(idd).getid()][Clientlist.get(b).getid()]
+													.equals("false")) {
+										Message[Clientlist.get(b).getid()][Clientlist.get(idd).getid()] = message;
+										update(Clientlist.get(b).gettk(), Clientlist.get(idd).gettk(), message);
 
-									String tk1 = Message[Clientlist.get(idd).getid()][Clientlist.get(b).getid()];
-									if (tk1.equals("")) {
-										tk1 = "";
-										Message[Clientlist.get(idd).getid()][Clientlist.get(b).getid()] = st;
-									} else
-										Message[Clientlist.get(idd).getid()][Clientlist.get(b).getid()] = tk1
-												+ "\r\n\r\n" + st;
-									System.out.println(Message[Clientlist.get(idd).getid()][Clientlist.get(b).getid()]);
-									// System.out.println(Clientlist.get(idd).sc.getOutputStream());
-									update(Clientlist.get(idd).gettk(), Clientlist.get(b).gettk(),
-											Message[Clientlist.get(idd).getid()][Clientlist.get(b).getid()]);
-									System.out.println("1234");
+										String tk1 = Message[Clientlist.get(idd).getid()][Clientlist.get(b).getid()];
+										if (tk1.equals("")) {
+											tk1 = "";
+											Message[Clientlist.get(idd).getid()][Clientlist.get(b).getid()] = st;
+										} else
+											Message[Clientlist.get(idd).getid()][Clientlist.get(b).getid()] = tk1
+													+ "\r\n\r\n" + st;
+										// System.out.println(Message[Clientlist.get(idd).getid()][Clientlist.get(b).getid()]);
+										// System.out.println(Clientlist.get(idd).sc.getOutputStream());
+
+										update(Clientlist.get(idd).gettk(), Clientlist.get(b).gettk(),
+												Message[Clientlist.get(idd).getid()][Clientlist.get(b).getid()]);
+										// System.out.println("1234");
+									}
 									if (sockets.get(Clientlist.get(idd).gettk()) != null)
 										if (kiemtra(Clientlist, Clientlist.get(idd).sc, idd)) {
-											System.out.println(kiemtra(Clientlist, Clientlist.get(idd).sc, idd));
-											System.out.println(Clientlist.get(idd).sc.getOutputStream());
-											System.out.println(Clientlist.get(idd).sc.getInputStream());
 
 											DataOutputStream dos1 = new DataOutputStream(
 													Clientlist.get(idd).sc.getOutputStream());
@@ -416,8 +548,10 @@ public class Server {
 											// System.out.println(
 											// Message[Clientlist.get(idd).getid()][Clientlist.get(b).getid()]);
 											dos1.writeUTF(Clientlist.get(b).gettk());
+
 											dos1.writeUTF("chua end");
 											// System.out.println(Clientlist.get(b).gettk());
+
 										}
 									System.out.println(loop);
 								} catch (IOException e) {
@@ -427,18 +561,7 @@ public class Server {
 									loop = false;
 									OnlineClient.remove(this);
 									sockets.put(Clientlist.get(b).gettk(), null);
-									if (kiemtra(Clientlist, Clientlist.get(idd).sc, idd)) {
-										DataOutputStream dos1 = new DataOutputStream(
-												Clientlist.get(idd).sc.getOutputStream());
-										System.out.println(
-												Message[Clientlist.get(idd).getid()][Clientlist.get(b).getid()]);
-										dos1.writeUTF(Message[Clientlist.get(idd).getid()][Clientlist.get(b).getid()]);
-										// System.out.println(
-										// Message[Clientlist.get(idd).getid()][Clientlist.get(b).getid()]);
-										dos1.writeUTF(Clientlist.get(b).gettk());
-										dos1.writeUTF("end");
-										// System.out.println(Clientlist.get(b).gettk());
-									}
+
 								}
 
 							}
